f209653fbd12c1047fbbc8801dac585344b61e38
Piotr Narożnik <pnaroznik@gmail.com>
2020-12-08 07:18:03 +0100

    Day8

---
+++ 2020.Tests/Days/Day8Tests.cs
@@ -0,0 +1,30 @@
+using _2020.Days;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace _2020.Tests.Days
+{
+    public class Day8Tests : BaseDayTests<Day8>
+    {
+        public Day8Tests(ITestOutputHelper testOutputHelper) : base(testOutputHelper, new Day8())
+        {
+        }
+
+        [Theory]
+        [BulkFileData("TestCases/Day8_TestCasePack.txt")]
+        public void Part1_pack(string testCaseName, string expectedResult, string _, string fileContent)
+        {
+            var result = Day.Part1(ParseFileContent(fileContent));
+
+            Assert.True(expectedResult == result.ToString(), $"\"{testCaseName}\" test case expected: {expectedResult}, actual: {result}");
+        }
+
+        [Theory]
+        [BulkFileData("TestCases/Day8_TestCasePack.txt")]
+        public void Part2_Pack(string testCaseName, string _, string expectedResult, string fileContent)
+        {
+            var result = Day.Part2(ParseFileContent(fileContent));
+            Assert.True(expectedResult == result.ToString(), $"\"{testCaseName}\" test case expected: {expectedResult}, actual: {result}");
+        }
+    }
+}
---
+++ 2020/Days/Day8.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2020.Days
+{
+    public class Day8 : IDay<int, string[]>
+    {
+        public int Part1(string[] input)
+        {
+            var (accumulatorValue, _) = GetAccumulatorValue(Map(input));
+
+            return accumulatorValue;
+        }
+
+        public int Part2(string[] input)
+        {
+            var lines = Map(input);
+
+            for (var i = 0; i < lines.Length; ++i)
+            {
+                var line = lines[i];
+                var old = line.Instruction;
+                line.Instruction = old switch
+                {
+                    "jmp" => "nop",
+                    "nop" => "jmp",
+                    _ => old
+                };
+                // try the input
+                var (accumulatorValue, loopDetected) = GetAccumulatorValue(lines);
+                if (!loopDetected)
+                    return accumulatorValue;
+                line.Instruction = old;
+            }
+
+            throw new Exception("Cannot fix the data");
+        }
+
+        private static (int accumulatorValue, bool loopDetected) GetAccumulatorValue(IReadOnlyList<Line> lines)
+        {
+            var accumulatorValue = 0;
+            var isLoopDetected = false;
+
+            var loopDetector = new HashSet<int>();
+            var i = 0;
+            while (i < lines.Count)
+            {
+                if (loopDetector.Contains(i))
+                {
+                    isLoopDetected = true;
+                    break;
+                }
+
+                loopDetector.Add(i);
+                var line = lines[i];
+                switch (line.Instruction)
+                {
+                    case "acc":
+                        accumulatorValue += line.Value;
+                        i++;
+                        break;
+                    case "jmp":
+                        i += line.Value;
+                        break;
+                    case "nop":
+                        i++;
+                        break;
+                    default: throw new Exception($"Unknown instruction- {line.Instruction}");
+                }
+
+            }
+
+            return (accumulatorValue, isLoopDetected);
+        }
+
+        private static Line[] Map(string[] input)
+        {
+            return input.Select(x =>
+            {
+                var lineSplit = x.Split(' ');
+                var instruction = lineSplit[0];
+                var value = int.Parse(lineSplit[1]);
+
+                return new Line(instruction, value);
+            }).ToArray();
+        }
+
+        private class Line
+        {
+            public string Instruction { get; set; }
+            public int Value { get; }
+
+            public Line(string instruction, int value)
+            {
+                Instruction = instruction;
+                Value = value;
+            }
+        }
+    }
+}
