e0c713f7c0761903d4babbe68ec02ccb9592c84d
Piotr Narożnik <pnaroznik@gmail.com>
2020-12-15 13:07:40 +0100

    Day15

---
+++ 2020/Days/Day15.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace _2020.Days
+{
+    public class Day15 : DayBase, IDay<long, string>
+    {
+        public long Part1(string input)
+        {
+            var numbers = input.Split(',').Select(int.Parse).ToList();
+
+            return Play(numbers, 2020);
+        }
+
+        private static long Play(IReadOnlyCollection<int> numbers, int iterations)
+        {
+            var dict = numbers
+                .Select((x, i) => (key: x, value: i + 1))
+                .ToDictionary(x => x.key, x => (lastIndex: x.value, lastBeforeIndex: (int?)null));
+
+
+            var lastNumberSpoken = numbers.Last();
+            for (var i = numbers.Count + 1; i <= iterations; i++)
+            {
+                var (lastIndex, lastBeforeIndex) = dict[lastNumberSpoken];
+                var newNumberSpoken = lastBeforeIndex == null ? 0 : lastIndex - lastBeforeIndex ?? 0;
+
+                dict[newNumberSpoken] = dict.ContainsKey(newNumberSpoken)
+                    ? dict[newNumberSpoken] = (i, dict[newNumberSpoken].lastIndex)
+                    : dict[newNumberSpoken] = (i, null);
+
+                lastNumberSpoken = newNumberSpoken;
+            }
+
+            return lastNumberSpoken;
+        }
+
+        public long Part2(string input)
+        {
+            var numbers = input.Split(',').Select(int.Parse).ToList();
+
+            return Play(numbers, 30000000);
+        }
+    }
+}
